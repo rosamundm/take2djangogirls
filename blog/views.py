ec30195efef1eb4265f9f7328dea0ed7b2190406
Rosamund <rsmnd.mthr@gmail.com>
2019-08-17 22:19:14 +0200

    faffing around with views and models

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -6,4 +7,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
