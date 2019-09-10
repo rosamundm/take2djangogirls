579ca655a309e7215c7649c342f19faf77dff912
Rosamund <rsmnd.mthr@gmail.com>
2019-09-10 19:56:56 +0200

    changed admin path

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('9admin1', admin.site.urls),
     path('', include('blog.urls')),
 ]
