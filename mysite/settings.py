43a8ccbd685192b39da62e28e414db43c869bb3a
Rosamund <rsmnd.mthr@gmail.com>
2019-09-10 20:20:05 +0200

    readding secret key experiment

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'removed_for_security_purposes_see_footer'
+SECRET_KEY = 'm(e)$wdtsm7x1@foagvjanuo=t3y+3gxb*s@suqmp%kj9)#4^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
