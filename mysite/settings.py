2097e5b3f36f4d139aa2bd0e216c22e8097cf1f0
Rosamund <rsmnd.mthr@gmail.com>
2019-09-12 15:53:08 +0200

    pip upgraded, celery installed, debug = true

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'removed_for_security'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
 # SESSION_COOKIE_SECURE = True
 
