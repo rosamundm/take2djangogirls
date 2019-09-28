a3af9b47749c8be25e07c7dc8497be01a67af2f2
Rosamund <rsmnd.mthr@gmail.com>
2019-09-28 18:14:34 +0200

    debug to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'removed_for_security'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SESSION_COOKIE_SECURE = True
 
