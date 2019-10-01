651fad103441b4f510f2fa1a81e97c59b533eba8
Rosamund <rsmnd.mthr@gmail.com>
2019-10-01 20:40:55 +0200

    added slash to virtual env in git ignore

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-#('mysite/', include 'mysite.settings') <-line 18
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
